f6fee8f8a61be4dddc7913b3bc0ce10db1a429ed
Daniel Rees <daniel_james_rees@hotmail.com>
2024-09-13 11:38:00 -0400

    rework some of the test_scenes, not that this commit might not be stable on it's own, trying to clean up a bunch of changes

--- test_scenes/components/resource_node.gd
+++ test_scenes/components/resource_node.gd
@@ -1,10 +1,10 @@
 class_name ResourceNode extends StaticBody2D
 
-@export var destroy_animation: String
+@export var destroy_animation: String = "destroy"
 @export var atlas_coord: int = 0
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
-var contents: Contents
+var contents: Inventory
 var animation: AnimationPlayer
 
 func _ready() -> void:
@@ -12,7 +12,7 @@ func _ready() -> void:
 	for child in get_children():
 		if child is HitBox:
 			child.empty_health.connect(drop_items)
-		if child is Contents:
+		if child is Inventory:
 			contents = child
 		if child is AnimationPlayer:
 			animation = child
@@ -20,17 +20,13 @@ func _ready() -> void:
 func drop_items() -> void:
 	if !contents:
 		return
-	var items = contents.drop_contents()
+	var items = contents.drop_all()
 	for item in items:
-		var quantity = 1
-		if item is ItemStack:
-			quantity = item.quantity
 		# drop_contents.emit(Collectible.from_item(item), quantity)
 		var parent = get_parent()
 		var col = Collectible.from_item(item)
 		col.drop_to(parent, position)
 	# some sort of animation perhaps
-	if animation:
+	if animation and destroy_animation:
 		animation.play(destroy_animation)
 		await animation.animation_finished
-	queue_free()
