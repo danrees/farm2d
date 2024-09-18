13e1090b440ceee0e86cad8dcab435f85d2f5398
Daniel Rees <daniel_james_rees@hotmail.com>
2024-09-18 13:34:14 -0400

    working on map layers and trying to figure out a better way to do it

---
+++ components/map/layer.gd
@@ -0,0 +1,1 @@
+class_name Layer extends Node2D
--- components/map/tile_map_layer.gd
+++ components/map/tile_map_layer.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends TileMapLayer
 
 const TERRAIN_SET = 0
 
@@ -11,12 +11,8 @@ const SOIL = 3
 const WET_SOIL = 0
 const SEEDS = 0
 
-@onready var terrain: TileMapLayer = $Terrain
-@onready var watered: TileMapLayer = $Status
-@onready var planted: TileMapLayer = $Planted
-
 func _check_property(prop: String, coords: Vector2i) -> bool:
-	var td = terrain.get_cell_tile_data(coords)
+	var td = get_cell_tile_data(coords)
 	return td.get_custom_data(prop) as bool
 
 func _find_type(prop: String, coords: Array[Vector2i]) -> Array[Vector2i]:
@@ -27,27 +23,20 @@ func _find_type(prop: String, coords: Array[Vector2i]) -> Array[Vector2i]:
 	return to_return
 	
 func tile_coords(local_coords: Vector2) -> Vector2i:
-	return terrain.local_to_map(local_coords)
+	return local_to_map(local_coords)
 			
 func till_soil(coords: Array[Vector2i]) -> void:
 	var to_till = _find_type(TILLABLE, coords)
-	terrain.set_cells_terrain_connect(to_till, TERRAIN_SET, SOIL )
+	set_cells_terrain_connect(to_till, TERRAIN_SET, SOIL )
 	
 func water_seed(coords: Array[Vector2i]) -> void:
 	var to_water = _find_type(WATERABLE, coords)
-	planted.set_cells_terrain_connect(to_water, TERRAIN_SET, WET_SOIL)
+	set_cells_terrain_connect(to_water, TERRAIN_SET, WET_SOIL)
 	
 func plant_seed(coords: Array[Vector2i], seed: Seed ) -> void:
 	var to_plant = _find_type(PLANTABLE, coords)
 	for c in coords:
-		var td = planted.get_cell_tile_data(c)
+		var td = get_cell_tile_data(c)
 		if td.get_custom_data("plant") == null:
 			td.set_custom_data("plant", seed.to_plant())
 			# planted.set_cells_terrain_connect(to_plant, TERRAIN_SET, SEEDS)
-	
-func _on_days_end() -> void:
-	for coords in planted.get_used_cells():
-		var td = planted.get_cell_tile_data(coords)
-		var plant = td.get_custom_data("plant")
-		if plant is Seed:
-			plant.grow(1)
